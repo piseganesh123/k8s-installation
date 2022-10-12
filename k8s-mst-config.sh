1bf0d5e0c3006a7324a754ae8536d5a7e4085993
piseganesh123 <piseganesh123@gmail.com>
2022-10-12 10:37:41 +0000

    updated docker installation instructions

--- k8s-mst-config.sh
+++ k8s-mst-config.sh
@@ -65,10 +65,6 @@ install_supp_tools() {
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
   
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
   sudo apt-get update
   sudo apt-get install -y docker-ce=5:20.10.18~3-0~ubuntu-focal docker-ce-cli=5:20.10.18~3-0~ubuntu-focal containerd.io=1.4.11-1
 
