d515625749a79c89270fa224504004f58967f428
piseganesh123 <piseganesh123@gmail.com>
2022-11-05 11:46:53 +0000

    updated containerd config

--- k8s-supp-tool-config.sh
+++ k8s-supp-tool-config.sh
@@ -45,15 +45,14 @@ overlay
 br_netfilter
 EOF
   
-  sudo modprobe overlay
-  sudo modprobe br_netfilter
-
 cat <<EOF | sudo tee /etc/sysctl.d/kubernetes.conf 
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF 
 
+  sudo modprobe overlay
+  sudo modprobe br_netfilter
   sudo sysctl --system
 
   #configure  container runtime 
