7cf74319c1b690c84f42772a5c333544115c74dc
piseganesh123 <piseganesh123@gmail.com>
2023-08-27 08:26:23 +0000

    reset master config

--- reset-k8s-master.sh
+++ reset-k8s-master.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
 #set -e
+
+ADV_ADDR="172.16.16.100"
+POD_NW_CIDR="192.168.0.0/16"
+CP_ENDPOINT="kmaster"
+
 deploy_k8s_cluster() {
   echo "=========== In k8s cluster deploy function =========="
   # === install Kubernetes
     
   kubeadm config images pull >/dev/null 2>&1
   
-  sudo kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=10.244.0.0/16
+#  sudo kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=10.244.0.0/16
+    sudo kubeadm init --apiserver-advertise-address=${ADV_ADDR} --pod-network-cidr=${POD_NW_CIDR} \
+  --control-plane-endpoint=${CP_ENDPOINT}
+
   #===wait while k8s comps are getting created
   #== to taint - run $kubectl taint nodes master-node key1=value1:NoSchedule
   kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
@@ -44,6 +52,13 @@ re_configure_user(){
   echo "==== Reconfigured user student01"
 }
 
+enable_master_deploy()
+{
+  kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+  echo "=== workload can be deployed on Master node now  ==="
+}
+
+
 main() {
   echo "=========== In reset function =========="
   # == install supporting tools like docker
@@ -52,6 +67,7 @@ main() {
   deploy_k8s_cluster
   #== deploy flannel n/w
   deploy_network
+  enable_master_deploy
   re_configure_user
 }
 
