97e584685f8e65a83d65fae2df17a56da90f307f
piseganesh123 <piseganesh123@gmail.com>
2023-09-03 08:36:02 +0000

    updated docker setup

--- labenv-doc-compose/doc-bootstrap-tool-config.sh
+++ labenv-doc-compose/doc-bootstrap-tool-config.sh
@@ -33,37 +33,12 @@ install_docker_supp_tools() {
 
 configure_host()
 {
-# === configure_hosts_file
-cat >>/etc/hosts<<EOF
-172.16.16.100   kmaster     kmaster.local
-172.16.16.101   kworker1    kworker1
-172.16.16.101   kworker1    kworker1.local
-EOF
-sed -e '/^.*ubuntu2204.*/d' -i /etc/hosts
 
 #=== configure_firewall
   echo "==== Stop and Disable firewall =="
   systemctl disable --now ufw >/dev/null 2>&1
 
-# === disable swap
-  sudo swapoff -a
-  sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-
-# ===== configure OS modules
-  sudo modprobe overlay
-  sudo modprobe br_netfilter
-  sudo sysctl --system >/dev/null 2>&1
-
-# ===== configure container runtime and kubernetes
-cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
-overlay
-br_netfilter
-EOF
-
-#===== enable ip forwarding
-  echo 1 > /proc/sys/net/ipv4/ip_forward
-
-echo "==== Configured host === "
+  echo "==== Configured host === "
 }
 
 configure_user()
