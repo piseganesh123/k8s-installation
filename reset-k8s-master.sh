036f99d07f26685cce3f88023fb439bc2b5825b5
piseganesh123 <piseganesh123@gmail.com>
2023-09-05 08:59:21 +0000

    updated reset master code

--- reset-k8s-master.sh
+++ reset-k8s-master.sh
@@ -9,15 +9,15 @@ deploy_k8s_cluster() {
   echo "=========== In k8s cluster deploy function =========="
   # === install Kubernetes
     
-  kubeadm config images pull >/dev/null 2>&1
+  sudo kubeadm config images pull >/dev/null 2>&1
   
 #  sudo kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=10.244.0.0/16
-    sudo kubeadm init --apiserver-advertise-address=${ADV_ADDR} --pod-network-cidr=${POD_NW_CIDR} \
+  sudo kubeadm init --apiserver-advertise-address=${ADV_ADDR} --pod-network-cidr=${POD_NW_CIDR} \
   --control-plane-endpoint=${CP_ENDPOINT}
 
   #===wait while k8s comps are getting created
   #== to taint - run $kubectl taint nodes master-node key1=value1:NoSchedule
-  kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
+  sudo kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
   echo "==== deployed k8s cluster ===="
 }
 
@@ -39,13 +39,13 @@ configure_host(){
 }
 hard_reset_k8s()
 {
-    kubeadm reset -f
+    sudo kubeadm reset -f
 }
 
 re_configure_user(){
   rm /home/student01/.kube/config
-  cp -i /etc/kubernetes/admin.conf /home/student01/.kube/config
-  chown student01:student01 /home/student01/.kube/config
+  sudo cp -i /etc/kubernetes/admin.conf /home/student01/.kube/config
+  sudo chown student01:student01 /home/student01/.kube/config
 
   #source <(kubectl completion bash)
   echo "source <(kubectl completion bash)" >> /home/student01/.bashrc
