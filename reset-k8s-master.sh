eadc427902d9fe4859c272a4ccc5fbbee1a742d3
piseganesh123 <piseganesh123@gmail.com>
2023-09-08 19:06:20 +0530

    Update reset-k8s-master.sh

--- reset-k8s-master.sh
+++ reset-k8s-master.sh
@@ -19,7 +19,7 @@ deploy_k8s_cluster() {
   #== to taint - run $kubectl taint nodes master-node key1=value1:NoSchedule
 
 #  echo $(kubeadm token create --print-join-command) | sudo tee  /joincluster.sh
-  sudo bash -c "echo $(kubeadm token create --print-join-command) > /joincluster.sh"
+  sudo bash -c "echo $(kubeadm token create --print-join-command) >> /joincluster.sh"
   echo "==== deployed k8s cluster ===="
 }
 
