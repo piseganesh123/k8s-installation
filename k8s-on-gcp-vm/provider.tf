5bc81e819ace0ed2f7c1daf1aad6eac837151192
piseganesh123 <piseganesh123@gmail.com>
2023-09-11 08:31:57 +0000

    updated variables

--- k8s-on-gcp-vm/provider.tf
+++ k8s-on-gcp-vm/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.24.0"
+      version = "4.73.2"
     }
     local = {
       source  = "hashicorp/local"
