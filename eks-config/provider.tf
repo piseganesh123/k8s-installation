5569bf4e4b629f9b973ca8a4e129525784df97af
piseganesh123 <piseganesh123@gmail.com>
2024-02-15 11:05:05 +0000

    eks configuration

---
+++ eks-config/provider.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+#  region = "ap-south-1"
+  region = var.aws_region
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+  }
+}
---
+++ eks-config/vpc.tf
@@ -0,0 +1,25 @@
+# GCP infrastructure resources
+
+resource "tls_private_key" "global_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "local_sensitive_file" "ssh_private_key_pem" {
+  filename        = "${path.module}/id_rsa_${formatdate("MM-DD", timestamp())}"
+  content         = tls_private_key.global_key.private_key_pem
+  file_permission = "0600"
+}
+
+resource "local_file" "ssh_public_key_openssh" {
+  filename = "${path.module}/id_rsa.pub"
+  content  = tls_private_key.global_key.public_key_openssh
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "trn-main"
+  }
+}
